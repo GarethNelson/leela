15f5452a5f06711d89c201fc2a8ba3e276413103
Gareth Nelson <gareth@garethnelson.com>
2016-08-25 04:56:04 +0100

    misc cleanup

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 export PATH=$PWD/toolchain/x86_64-elf-5.3.0-Linux-x86_64/bin/:$PATH
+
+echo Building xv6 kernel
 make all
 
+echo Building root filesystem image
+mkdir -p build/sysroot
+cp README.md build/sysroot/
+cp build/kernel.elf build/sysroot/vmunix
+virt-make-fs --type=ext2 build/sysroot fs.img
