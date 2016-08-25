42a9d419e4692d42a5a3a12d5e927e82e4576248
Gareth Nelson <gareth@garethnelson.com>
2016-08-25 11:37:31 +0100

    import multiboot header

--- build.sh
+++ build.sh
@@ -6,9 +6,9 @@ echo Building newlib for kernel
 mkdir -p newlib/build
 mkdir -p build/sysroot
 pushd newlib/build
-CFLAGS="-fPIC -mcmodel=large" .././configure --target=x86_64-elf-xv6 --prefix=/usr CC_FOR_TARGET=x86_64-elf-gcc AS_FOR_TARGET=x86_64-elf-gcc LD_FOR_TARGET=x86_64-elf-ld AR_FOR_TARGET=x86_64-elf-ar RANLIB_FOR_TARGET=x86_64-elf-ranlib CFLAGS="-fPIC -mcmodel=large"
-make all
-make DESTDIR=`realpath ../../build/sysroot` install
+#CFLAGS="-fPIC -mcmodel=large" .././configure --target=x86_64-elf-xv6 --prefix=/usr CC_FOR_TARGET=x86_64-elf-gcc AS_FOR_TARGET=x86_64-elf-gcc LD_FOR_TARGET=x86_64-elf-ld AR_FOR_TARGET=x86_64-elf-ar RANLIB_FOR_TARGET=x86_64-elf-ranlib CFLAGS="-fPIC -mcmodel=large"
+#make all
+#make DESTDIR=`realpath ../../build/sysroot` install
 popd
 
 cp -ar build/sysroot/usr/x86_64-elf-xv6/* build/sysroot/usr/
@@ -18,6 +18,6 @@ make all
 
 echo Building root filesystem image
 mkdir -p build/sysroot
-cp README.md build/sysroot/
+mkdir -p build/sysroot/dev
 cp build/kernel.elf build/sysroot/vmunix
 virt-make-fs --type=ext2 build/sysroot fs.img
